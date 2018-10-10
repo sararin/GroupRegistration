38c259323c6ab70148f86e484c2e12eac9bf904e
sararin <sararin@cock.li>
2018-10-10 10:59:36 -0400

    empty registration

--- main.py
+++ main.py
@@ -3,5 +3,15 @@ import json
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html', group = [{'group': "K1"}, {'group': "K2"}])
+
+@app.route("/register", methods=['GET', 'POST'])
+def registration():
+    name = request.form.get('name_input')
+    group = request.form.get('group_select')
+    return name+group
+
 if __name__ == "__main__":
     app.run()
